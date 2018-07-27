378ade39d5bfba5cde8f9ddd93d98ed209598d81
AnnaKenter <anna.retnek@gmail.com>
2018-07-27 16:35:18 +0200

    now trying a update

---
+++ example_pkg/example_pkg/print_example2.py
@@ -0,0 +1,2 @@
+def print_input(text):
+    print(text)
---
+++ example_pkg/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name="example_package",
+    version="0.0.1",
+    author="Kenter.Loctimize",
+    author_email="kenter@loctimize.com",
+    description="A small example package",
+    url="https://github.com/AnnaKenter/example_repo2.git",
+    packages=setuptools.find_packages()
+)
